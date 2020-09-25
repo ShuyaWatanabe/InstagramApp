720d0b1766de92d507f5b9a8ff9868bdb1db7dae
shuya watanabe <swatanabe990104@gmail.com>
2020-09-26 00:33:08 +0900

    まとめまで

---
+++ Instagram/PostData.swift
@@ -0,0 +1,43 @@
+//
+//  PostData.swift
+//  Instagram
+//
+//  Created by Shuya Watanabe on 2020/09/24.
+//  Copyright © 2020 Shuya Watanabe. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class PostData: NSObject {
+    var id: String
+    var name: String?
+    var caption: String?
+    var date: Date?
+    var likes: [String] = []
+    var isLiked: Bool = false
+    
+    init(document: QueryDocumentSnapshot) {
+        self.id = document.documentID
+        
+        let postDic = document.data()
+        
+        self.name = postDic["name"] as? String
+        
+        self.caption = postDic["caption"] as? String
+        
+        let timestamp = postDic["data"] as? Timestamp
+        self.date = timestamp?.dateValue()
+        
+        if let likes = postDic["likes"] as? [String] {
+            self.likes = likes
+        }
+        
+        if let myid = Auth.auth().currentUser?.uid {
+            
+            if self.likes.firstIndex(of: myid) != nil {
+                self.isLiked = true
+            }
+        }
+    }
+}
